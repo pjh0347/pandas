ffe1dfaacb5044f221a663c1bc6221409777b6fa
Jinho Park <pjh0347@gmail.com>
2017-11-30 18:48:17 +0900

    added pandas examples

--- apply_map.py
+++ apply_map.py
@@ -8,15 +8,18 @@ df = pd.DataFrame([[1,1,1],[3,2,3],[3,2,4],[5,3,1],[1,np.nan,3],[1,np.nan,3],[1,
 print "original"
 print df
 
-print "df.apply(np.sum) : on a row/column basis of a DataFrame"
+print "df.apply(np.sum) : apply function to each column"
 print df.apply(np.sum)
 
-print "df.apply(np.sum, axis=1) : on a row/column basis of a DataFrame"
+print "df.apply(np.sum, axis=1) : apply function to each crow"
 print df.apply(np.sum, axis=1)
 
 print "df['a'].map(lambda x: x == 1) : element-wise on a Series."
 print df['a'].map(lambda x: x == 1)
 
-print "df.applymap(lambda x: pd.isnull(x)) : element-wise on a DataFrame"
+print "df['a'].apply(lambda x: x == 1) : For applying more complex functions(NumPy/Python) on a Series"
+print df['a'].apply(lambda x: x == 1)
+
+print "df.applymap(lambda x: pd.isnull(x)) : Apply a function elementwise on a whole DataFrame"
 print df.applymap(lambda x: pd.isnull(x))
 
